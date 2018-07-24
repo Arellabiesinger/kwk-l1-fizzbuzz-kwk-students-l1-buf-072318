19522fcf88b76d682ab4f167e7a86427706ed36e
Arellabiesinger <arellabiesinger@gmail.com>
2018-07-24 18:58:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
+    elsif number % 3 == 0 && number % 5 == 0 
+  "fizzbuzz"
   if number % 3 == 0 
     "Fizz"
   elsif number % 5 == 0 
     "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0 
-  "fizzbuzz"
   end
 end
 
